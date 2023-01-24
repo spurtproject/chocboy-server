3462daff0f4d169f7271c55acf9a852e6ddb7463
Ayooluwa Adeleke <51849584+PolyMath-Coder@users.noreply.github.com>
2023-01-24 13:44:51 +0100

    initial commit

---
+++ src/config/keys.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+module.exports = {
+  //   NODE_ENV: process.env.NODE_ENV || 'development',
+  PORT: process.env.PORT || 3000,
+  //   DB_URL: process.env.DB_URL,
+  //   JWT_SECRET: process.env.JWT_SECRET,
+  //   SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
+};
---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const { json, urlencoded } = express;
+require('dotenv').config();
+const app = express();
+const { PORT } = require('./config/keys');
+
+app.use(json());
+app.use(urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res
+    .status(201)
+    .json({ status: true, message: 'Welcome to Chocboy Index Page' });
+});
+
+app.listen(3000, () => {
+  console.log(`Server now live at port ${PORT}`);
+});
